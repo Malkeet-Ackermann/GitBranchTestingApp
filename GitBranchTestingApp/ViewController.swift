d3f6d84f0c0e7e9a2da2c6b548da860b99a6fc8a
Malkeet_Levi <malkeetsinghworkspace@gmail.com>
2022-12-30 15:38:24 +0530

    Initial Commit

---
+++ GitBranchTestingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitBranchTestingApp
+//
+//  Created by malkeet on 30/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
